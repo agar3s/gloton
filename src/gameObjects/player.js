b4990f109a49148811f26dbbd81d4b1340fcf8ba
Agar3s <ekeisco@gmail.com>
2018-08-11 18:36:24 -0500

    grab items and pull them

--- src/gameObjects/player.js
+++ src/gameObjects/player.js
@@ -45,7 +45,7 @@ export default class Player {
     // get input from player
     let y = this.keys.W.isDown?-1:this.keys.S.isDown?1:0
     let x = this.keys.A.isDown?-1:this.keys.D.isDown?1:0
-    let speed = gs.stats.player.speed
+    let speed = gs.stats.player.speed * (this.hookedItem?0.7:1)
     
     this.sprite.body.setVelocity(0)
     this.sprite.body.setVelocityX(x*speed)
@@ -61,10 +61,11 @@ export default class Player {
       this.handSprite.x = this.sprite.x
       this.handSprite.y = this.sprite.y
     } else {
-      this.updateHand(angle)
+      angle = this.updateHand(angle)
       this.drawHand()
     }
 
+    this.handSprite.rotation = angle
     // draw the ray
     if(gs.stats.game.debug) {
       this.drawDebug(angle)
@@ -112,8 +113,17 @@ export default class Player {
   }
 
   updateHand (angle) {
-    this.hand.lenght += gs.stats.player.chainSpeed*(this.hand.going?1:-1)
 
+    if(this.hand.going) {
+      return this.updatePushingHand(angle)
+    } else {
+      return this.updatePullingHand(angle)
+    }
+
+  }
+
+  updatePushingHand (angle) {
+    this.hand.lenght += gs.stats.player.chainSpeed*(this.hand.going?1:-1)
     this.handSprite.x = (Math.cos(angle))*this.hand.lenght + this.sprite.x
     this.handSprite.y = (Math.sin(angle))*this.hand.lenght + this.sprite.y
 
@@ -124,24 +134,26 @@ export default class Player {
       this.hand.lenght = gs.stats.player.chainLength
       this.hand.going = false
     }
+    return angle
+  }
+  updatePullingHand (angle) {
+    if(this.hookedItem) {
+      angle = Phaser.Math.Angle.Between(this.sprite.x, this.sprite.y, this.handSprite.x, this.handSprite.y)
+      
+      this.hookedItem.sprite.body.setVelocityX(Math.cos(angle + Math.PI)*40*gs.stats.player.chainSpeed)
+      this.handSprite.body.setVelocityX(Math.cos(angle + Math.PI)*40*gs.stats.player.chainSpeed)
+      this.hookedItem.sprite.body.setVelocityY(Math.sin(angle + Math.PI)*40*gs.stats.player.chainSpeed)
+      this.handSprite.body.setVelocityY(Math.sin(angle + Math.PI)*40*gs.stats.player.chainSpeed)
+    }else {
+      this.hand.lenght += gs.stats.player.chainSpeed*(this.hand.going?1:-1)
+      this.handSprite.x = (Math.cos(angle))*this.hand.lenght + this.sprite.x
+      this.handSprite.y = (Math.sin(angle))*this.hand.lenght + this.sprite.y
+    }
     if(this.hand.lenght <= 0) {
       this.hand.locked = false
       this.graphics.clear()
-      
-      if(this.hookedItem) {
-        this.hookedItem.sprite.body.setVelocityX(0)
-        this.hookedItem.sprite.body.setVelocityY(0)
-        this.hookedItem = undefined
-      }
     }
-
-    if(this.hookedItem) {
-      // a vector in direction to the player
-      let newAngle = angle + Math.PI
-
-      this.hookedItem.sprite.body.setVelocityX(Math.cos(newAngle)*100)
-      this.hookedItem.sprite.body.setVelocityY(Math.sin(newAngle)*100)
-    }
+    return angle
   }
 
   drawHand() {
@@ -176,6 +188,17 @@ export default class Player {
     this.hand.going = false
     console.log('hook item!!')
     // put a delay before to start pullingout
+  }
 
+  grabItem(item) {
+    if(item !== this.hookedItem) return
+    console.log('save item!!')
+    this.hand.locked = false
+    this.graphics.clear()
+    this.hookedItem.sprite.body.setVelocityX(0)
+    this.handSprite.body.setVelocityX(0)
+    this.hookedItem.sprite.body.setVelocityY(0)
+    this.handSprite.body.setVelocityY(0)
+    this.hookedItem = undefined
   }
 }
