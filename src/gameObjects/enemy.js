5cc64e5d6adc58c335f80db0d08efdf59d312dee
Agar3s <ekeisco@gmail.com>
2018-08-13 11:24:47 -0500

    adds the waking state for skeleton

--- src/gameObjects/enemy.js
+++ src/gameObjects/enemy.js
@@ -1,10 +1,30 @@
 import Item from './item'
 
+const STATUS = {
+  REST: 0,
+  WAKE: 1,
+  IDLE: 2
+}
 export default class Enemy extends Item {
   constructor(params) {
     super(params)
 
-    if(!this.scene.anims.anims.entries['skeleton-idle']){
+    if(!this.scene.anims.anims.entries['skeleton-resting']){
+      this.scene.anims.create({
+        key: 'skeleton-resting',
+        frames: this.scene.generateFrameNames('characters/npc', 'skeleton-resting', 1),
+        frameRate: 10,
+        repeat: 0
+      })
+      this.scene.anims.create({
+        key: 'skeleton-wake',
+        frames: this.scene.generateFrameNames('characters/npc', 'skeleton-wake', 4),
+        repeat: 0
+      })
+      this.scene.anims.anims.get('skeleton-wake').frames.forEach((frame, index)=>{
+        frame.duration = 125
+      })
+
       this.scene.anims.create({
         key: 'skeleton-idle',
         frames: this.scene.generateFrameNames('characters/npc', 'skeleton-idle', 8),
@@ -13,7 +33,23 @@ export default class Enemy extends Item {
       })
     }
     this.tint = 0xffffff
-    this.anims.play('skeleton-idle')
+    this.anims.play('skeleton-resting')
+    this.status= STATUS.REST
+
+    setTimeout(()=>{
+      this.wake()
+    }, (~~(Math.random()*10000)) + 3000)
+  }
+
+  wake() {
+    if(this.status === STATUS.REST)
+    this.status = STATUS.WAKE
+    console.log(this.anims.play('skeleton-wake'))
+    this.on('animationcomplete', (animation, frame) => {
+      this.status = STATUS.IDLE
+      this.anims.play('skeleton-idle')
+      this.off('animationcomplete')
+    }, this)
 
   }
 
