5753f668f97acaeac0d24aa8ab9c1e61c55f214b
Agar3s <ekeisco@gmail.com>
2018-08-13 11:09:54 -0500

    finish creating anim

--- src/gameObjects/enemy.js
+++ src/gameObjects/enemy.js
@@ -3,12 +3,15 @@ import Item from './item'
 export default class Enemy extends Item {
   constructor(params) {
     super(params)
-    this.scene.anims.create({
-      key: 'skeleton-idle',
-      frames: this.scene.generateFrameNames('characters/npc', 'skeleton-idle', 8),
-      frameRate: 10,
-      repeat: -1
-    })
+
+    if(!this.scene.anims.anims.entries['skeleton-idle']){
+      this.scene.anims.create({
+        key: 'skeleton-idle',
+        frames: this.scene.generateFrameNames('characters/npc', 'skeleton-idle', 8),
+        frameRate: 10,
+        repeat: -1
+      })
+    }
     this.tint = 0xffffff
     this.anims.play('skeleton-idle')
 
@@ -20,6 +23,9 @@ export default class Enemy extends Item {
   }
   setProperties() {
     super.setProperties()
+    this.setDrag(300, 300)
+    this.setBounce(0.1, 0.1)
+    this.setFriction(4, 4)
   }
 
   grab() {
